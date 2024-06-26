f45686f5dc699158fd72c2c89ff8f01d70d10726
onakrainikoff <onakrainikoff@gmail.com>
2024-06-26 15:20:51 +0300

    work commit: impl backend

--- backend/src/main/java/ru/on8off/example/crud/backend/repository/StoredObjectRepository.java
+++ backend/src/main/java/ru/on8off/example/crud/backend/repository/StoredObjectRepository.java
@@ -3,6 +3,8 @@ package ru.on8off.example.crud.backend.repository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.on8off.example.crud.backend.repository.entity.StoredObject;
 
+@RepositoryRestResource
 public interface StoredObjectRepository extends CrudRepository<StoredObject, Integer>, PagingAndSortingRepository<StoredObject, Integer> {}
